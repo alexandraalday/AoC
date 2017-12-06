332f565a37c8fc03185b2487c2b40e3273f4e8e8
alexandraalday <alexandra.alday@gmail.com>
2017-12-05 20:20:40 -0800

    day four notes

---
+++ 2017/day04.js
@@ -0,0 +1,7 @@
+const passphrases = document.body.textContent.trim().split('\n');
+const removeRepeats = (w, _, ws) => ws.filter(v => v === w).length === 1;
+const sortLetters = w => [...w].sort().join('');
+const isValid = f => ph => ph.split(' ').map(f).every(removeRepeats);
+const count = f => passphrases.filter(isValid(f)).length;
+
+console.log([w => w, sortLetters].map(count));
